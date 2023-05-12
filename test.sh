aab2d81f5a1cd69e84f0edb65fba12ce8a8f4a4b
Gocho1234 <goncalo.barias@gmail.com>
2023-05-12 19:57:36 +0100

    Add public test instances published on fénix

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# get all files ending with .txt from ./tests/
+# and run them, comparing them with their respective .out
+# if the output is the same, print "Test _ SUCCESS" in green,
+# otherwise print "Test _ FAILED" in red
+
+files=$(ls ./tests/ | grep ".txt")
+
+for file in $files
+do
+  python ./src/bimaru.py < ./tests/$file > /tmp/bimaru.out
+  output=$(cat /tmp/bimaru.out)
+  output_file=$(echo $file | sed 's/txt/out/')
+  expected_output=$(cat ./tests/$output_file)
+  if [ "$output" = "$expected_output" ]
+  then
+    echo -e "\e[32mTest $file SUCCESS\e[0m"
+  else
+    echo -e "\e[31mTest $file FAILED\e[0m"
+    # diff between /tmp/bimaru.out and ./$output_file
+    colordiff -u ./tests/$output_file /tmp/bimaru.out
+  fi
+done
